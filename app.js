ed0d3a3b13e60ea1316ae780bdb8d0eeadd2d492
Kimberly <kowens4@elon.edu>
2018-11-17 16:52:05 -0500

    installing postgres i think

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const errorHandler = require('errorhandler');
 
-//configure mongoDB???
+//Configure mongoose's promise to global promise
 mongoose.promise = global.Promise;
 
 //Configure isProduction variable
@@ -20,29 +20,23 @@ app.use(cors());
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(_dirname, 'public')));
-app.use(session({ secret: 'passport-tutorial', cookie: {maxAge: 6000}, resave: false, saveUninitialized: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
 if(!isProduction) {
   app.use(errorHandler());
 }
 
-//Configure mongoDB
+//Configure Mongoose
 mongoose.connect('mongodb://localhost/passport-tutorial');
 mongoose.set('debug', true);
 
-
-
-//Models & routes
-require('./models/Users');
-require('./config/passport');
-
 //Error handlers & middlewares
 if(!isProduction) {
-   app.use((err, req, res) => {
-     res.status(err.status || 500);
+  app.get((req, res, err) => {
+    res.status(err.status || 500);
 
-     res.json({
+    res.json({
       errors: {
         message: err.message,
         error: err,
@@ -51,11 +45,11 @@ if(!isProduction) {
   });
 }
 
-app.use(err, req, res) => {
-   res.status(err.status || 500);
+app.get((req, res, err) => {
+  res.status(err.status || 500);
 
-   res.json({
-     errors: {
+  res.json({
+    errors: {
       message: err.message,
       error: {},
     },
